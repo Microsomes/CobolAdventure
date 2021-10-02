faeac48dc4cd3c832d6bc69140c15d8dde5a35e0
tayyab <tayyab.javed@stickee.co.uk>
2021-10-02 10:46:51 +0100

    first cobol application

---
+++ helloworld.cob
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       DATE-WRITTEN. October 2 2021
+       DATA DIVISION. 
+       FILE SECTION. 
+       WORKING-STORAGE SECTION. 
+       PROCEDURE DIVISION.
+           DISPLAY "hello world!".
+           STOP RUN.
